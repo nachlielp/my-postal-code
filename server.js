bd9b6ce2e24e5f0d298440e6e4f7df48a7b38887
Nachliel <nachpok@gmail.com>
2025-04-24 15:07:29 +0300

    if missing api key show only based on current location

--- server.js
+++ server.js
@@ -5,19 +5,10 @@ const path = require("path");
 const app = express();
 const port = process.env.PORT || 3000;
 
-// Serve static files from the public directory
 app.use(express.static("public"));
 
-// Serve the HTML file
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "public", "index.html"));
-});
-
-// Create a route to expose the Google Maps API key
-app.get("/config", (req, res) => {
-  res.json({
-    GOOGLE_MAPS_API_KEY: process.env.GOOGLE_MAPS_API_KEY,
-  });
 });
 
 app.listen(port, () => {
